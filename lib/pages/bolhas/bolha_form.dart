1fdcb5c7ce082a6075f093fe428d1fba241213ec
rafael <rafael_oliveira500@hotmail.com>
2019-11-30 14:28:56 -0300

    fazendo o form de adicionar bolha como dialogo, add fancy dialog

---
+++ lib/pages/bolhas/bolha_form.dart
@@ -0,0 +1,52 @@
+// Define a custom Form widget.
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BolhasForm extends StatefulWidget {
+  @override
+  BolhasFormState createState() {
+    return BolhasFormState();
+  }
+}
+
+class BolhasFormState extends State<BolhasForm> {
+
+  TextEditingController _textFieldController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return AlertDialog(
+      title: Text('Adicionar Bolha'),
+      content: Container(
+        width: MediaQuery.of(context).size.width * 0.75,
+        height: MediaQuery.of(context).size.height * 0.15,
+        child: Column(
+          children: <Widget>[
+            TextField(
+              onChanged: (text) {
+                print("First text field: $text");
+              },
+              controller: _textFieldController,
+              decoration: InputDecoration(hintText: "Apelido"),
+            )
+          ],
+        ),
+      ),
+      actions: <Widget>[
+        new FlatButton(
+          child: new Text('Cancelar'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        new FlatButton(
+          child: new Text('Criar'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        )
+      ],
+    );
+  }
+}
